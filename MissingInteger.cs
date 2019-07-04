cc1e5736d98e6ccf99719a4d102b5071aaf74420
Mohamad Hussein Al Jarmak <mohamadjarmak@gmail.com>
2019-07-04 14:39:54 +0200

    Update MissingInteger.cs

--- MissingInteger.cs
+++ MissingInteger.cs
@@ -3,7 +3,6 @@ using System.Linq;
 
 class Solution {
     public int solution(int[] A) {
-        // write your code in C# 6.0 with .NET 4.5 (Mono)
         
             if (A.Max() <= 0)
                 return 1;
